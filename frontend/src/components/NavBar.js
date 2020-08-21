6b4891120edd74fbab295a89152cb2539c3fd7df
Ben Chan <chanben@bu.edu>
2020-08-21 12:17:16 -0400

    Changed api and navbar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/NavBar.css';
 
-export default class NavBar extends Component {
-  constructor() {
-    super();
-  }
+export default function NavBar() {
 
-  render() {
-    return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link class="navbar-brand" to="/">BU Course Tracker</Link>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
-            </li>
-            <li class="nav-item">
-              <Link class="nav-link" to="/">Log In</Link>
-            </li>
-          </ul>
-        </div>
+  return (
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link class="navbar-brand" to="/">BU Course Tracker</Link>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <Link class="nav-link" to="/">Home <span class="sr-only">(current)</span></Link>
+          </li>
+          <li class="nav-item">
+            <Link class="nav-link" to="/">Log In</Link>
+          </li>
+        </ul>
+      </div>
 
-      </nav>
-    )
-  }
+    </nav>
+  )
+
 }
